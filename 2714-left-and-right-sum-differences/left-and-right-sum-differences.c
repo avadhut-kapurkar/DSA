29fd88699d356ff2b742c9c26ea13cf54122d553
Avadhut kapurkar <avadhutkapurkar@gmail.com>
2026-06-06 19:01:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (73.81%) - LeetSync

---
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int* leftRightDifference(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+
+    int *answer = (int *)malloc(numsSize * sizeof(int));
+
+    int totalSum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        totalSum += nums[i];
+    }
+
+    int leftSum = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        int rightSum = totalSum - leftSum - nums[i];
+        answer[i] = abs(leftSum - rightSum);
+        leftSum += nums[i];
+    }
+
+    return answer;
+}
+
+/* Example Usage
+int main() {
+    int nums[] = {10, 4, 8, 3};
+    int n = sizeof(nums) / sizeof(nums[0]);
+    int returnSize;
+
+    int *result = leftRightDifference(nums, n, &returnSize);
+
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d ", result[i]);
+    }
+
+    free(result);
+    return 0;
+}
+*/
